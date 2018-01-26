2e4120b6c116297c6465d369673ccd9d91808b45
SatoshiTeraaki <terasakisatoshi.math@gmail.com>
2018-01-26 19:23:11 +0900

    modify euler

--- eulerMethod/euler.nim
+++ eulerMethod/euler.nim
@@ -1,13 +1,10 @@
-import times
-
 proc main()=
-  const N :int = 1000000
+  const N :int64 = 100000000
   const h: float64 = 0.00000004
   var y:array[N,float64]
-  var s = cpuTime()
+  y[0]=1.0
   for n in 0..<N-1:
     y[n+1]=y[n]-h*y[n]
-  var e = cpuTime()
-  echo "elapsed ", e-s, "[sec]"
+
 if isMainModule:
   main()
