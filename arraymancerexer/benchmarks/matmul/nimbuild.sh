199d5feb114747409f7ec596f71d9660ec663c62
terasakisatoshi <terasakisatoshi.math@gmail.com>
2018-06-02 23:07:56 +0900

    add benchmarks for arraymancer

---
+++ arraymancerexer/benchmarks/matmul/nimbuild.sh
@@ -0,0 +1,11 @@
+nim c -d:release \
+ --define:openmp \
+ --define:"blas=mkl_intel_lp64" \
+ --define:"lapack=mkl_intel_lp64" \
+ --clibdir:"/opt/intel/mkl/lib/intel64" \
+ --passL:"/opt/intel/mkl/lib/intel64/libmkl_intel_lp64.a" \
+ --passL:"-lmkl_core" \
+ --passL:"-lmkl_gnu_thread" \
+ --passL:"-lgomp" \
+ --dynlibOverride:"mkl_intel_lp64" \
+ integer_matmul.nim
